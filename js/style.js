fd68bc1c75d4d3a87fa1a3662e66b25f0e78d411
pabloart87 <pablo_ariel_toledo@hotmail.com>
2022-10-08 18:44:03 -0300

    agregue todas las carpetas

---
+++ js/style.js
@@ -0,0 +1,12 @@
+
+
+// Animación del header
+
+    window.addEventListener("scroll",
+        ()=>{
+            let header = document.querySelector("header");
+            header.classList.toggle("sticky",window.scrollY > 0)
+        }
+    )
+
+    
