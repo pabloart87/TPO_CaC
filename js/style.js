3da4dfe85deafd54d9b3c29cd55ffbbe5b508691
marcelo-la-rosa <barto86@gmail.com>
2022-10-22 16:42:02 -0300

    Actualización

--- js/style.js
+++ js/style.js
@@ -1,14 +1,39 @@
 
 
-// Animación del header
+// HEADER
 
     window.addEventListener("scroll",
         ()=>{
             let header = document.querySelector("header");
-            header.classList.toggle("sticky",window.scrollY > 0)
+            header.classList.toggle("barra_navegacion_fixed",window.scrollY > 0)
         }
     )
 
 
+
+
 // Slide Main //
+
+let slideIndex = 0;
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+  
     
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+  }
+
+
+  
