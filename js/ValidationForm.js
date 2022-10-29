f2b1b09485b103d7a5d353214a3c366a3619923b
marcelo-la-rosa <barto86@gmail.com>
2022-10-28 22:14:18 -0300

    arreglos finales

--- js/ValidationForm.js
+++ js/ValidationForm.js
@@ -8,33 +8,42 @@ function EnviarFormulario(){
     console.log('enviando formulario...');
 
     var MensajesError = [];
+    var msg = true;
     var regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
     var regexTel = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/
 
+
     // nombre
     if(nombre.value === null || nombre.value === ''){
         MensajesError.push('Debe ingresar nombre.')
+        msg = false;
     }
      
     // email
     if(email.value === null || email.value === ''){
         MensajesError.push('Debe ingresar un email.')
+        msg = false;
+
     }
     else if(!regexMail.test(email.value) ){
         MensajesError.push('Verifique formato de email')
+        msg = false;
     }
 
     // teléfono
 
     if(telefono.value === null || telefono.value === ''){
         MensajesError.push('Debe ingresar un teléfono.')
+        msg = false;
     }
     else if(!regexTel.test(telefono.value) ){
         MensajesError.push('Verifique teléfono')
+        msg = false;
     }
 
-    if(!MensajesError){
+    if(msg == true){
+
         alert('Formulario correctamente enviado');
         formulario.reset()
     }
@@ -42,6 +51,8 @@ function EnviarFormulario(){
     error.innerHTML = MensajesError.join(', ');
 
     // enviado.innerHTML = enviado('El formulario ha sido enviado correctamente.')
+
+    
 
     return false;
 }
---
+++ readme.txt
@@ -0,0 +1,21 @@
+Marcelo La Rosa
+Creación de archivos:
+	- Index.html
+	- Grid.css
+	- Responsive.css
+	- Styles.css
+	- Colocación de imágenes
+	- Style.js
+	- Header y Footer de todas las páginas
+	- Creacion de carousel y animación del header.
+
+Pablo Toledo
+Creación de archivos:
+	- Contacto.html
+	- Nosotros.html
+	- Op_clientes.html
+	- Pruductos.html
+	- Colocación de imágenes
+	- ValidationForm.js
+	- Style_contacto.css
+	- Validación de formulario.
